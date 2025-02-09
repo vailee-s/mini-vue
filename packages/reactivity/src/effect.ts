cb7c7fc801a780143721e609d0c7b92f404777d3
gssheng <vailee@126.com>
2025-02-09 18:57:00 +0800

    依赖清理

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -8,6 +8,11 @@ export function effect(fn, options?) {
   _effect.run();
   return _effect;
 }
+
+function preCleanUpEffect(effect) {
+  effect._depsLength = 0;
+  effect.trackId++; // 每次执行都会自增,如果当前同一个effect执行，id就是相同的
+}
 export let activeEffect;
 class ReactiveEffect {
   _trackId = 0; // 用于记录当前effect执行了几次
@@ -18,11 +23,15 @@ class ReactiveEffect {
   constructor(public fn, public scheduler) {}
   run() {
     if (!this.active) {
-      return this.fn(); // 不是激活状态，执行后什么都不用做
+      return this.fn(); // 不是激活状态的话，执行后什么都不用做
     }
     let lastEffect = activeEffect;
     try {
       activeEffect = this;
+
+      // 预先清理一下依赖
+      preCleanUpEffect(this);
+
       return this.fn(); // 收集依赖
     } finally {
       activeEffect = lastEffect;
@@ -30,11 +39,32 @@ class ReactiveEffect {
   }
 }
 
+function cleanDepEffect(dep, effect) {
+  dep.delete(effect);
+  if (dep.size === 0) {
+    dep.cleanup();
+  }
+}
 export function trackEffect(effect, dep) {
   // 将effect放到dep中
-  dep.set(effect, effect._trackId);
-  effect.deps[effect._depsLength++] = dep; // effect和dep关联起来
-  console.log('---', effect.deps);
+  // dep.set(effect, effect._trackId);
+  // effect.deps[effect._depsLength++] = dep; // effect和dep关联起来
+  // console.log('---', effect.deps);
+
+  // 需要重新的去收集依赖，将不需要的移除
+
+  if (dep.get(effect) !== effect._trackId) {
+    dep.set(effect, effect._trackId);
+  }
+  let oldDep = effect.deps[effect._depsLength];
+  if (oldDep !== dep) {
+    if (oldDep) {
+      cleanDepEffect(oldDep, effect);
+    }
+    effect.deps[effect._depsLength++] = dep;
+  } else {
+    effect._depsLength++;
+  }
 }
 
 export function triggerEffects(dep) {
